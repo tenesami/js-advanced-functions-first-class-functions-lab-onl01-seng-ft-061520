a6bad58e30a6692f9c3f17dedd93ceb5c3779442
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-11-03 17:38:47 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+// function actionApplyer(start, fns) {
+//     for (let i = 0; i < fns.length; i++) {
+//         start = fns[i](start);
+//     }
+//     return start;
+// }
+
+const returnFirstTwoDrivers = (arr) => {
+    return arr.slice(0, 2);
+}
+
+const returnLastTwoDrivers = (arr) => {
+    return arr.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers(), returnLastTwoDrivers()];
+
+//selectingDrivers.forEach(function())
